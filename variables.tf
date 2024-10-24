e1f594a9139f7996868b3128519d49851c77ea0b
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2024-10-24 16:24:28 +0100

    adding cloudfront and ES dashbaords
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards
    
    adding new dashboards

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "s3_shared" {
   type = string
 }
 variable "s3_preset" {
-  type = string
+  type = bool
 }
 variable "rds_dashbaord_name" {
   type = string
@@ -24,7 +24,7 @@ variable "rds_shared" {
   type = string
 }
 variable "rds_preset" {
-  type = string
+  type = bool
 }
 variable "mks_dashbaord_name" {
   type = string
@@ -36,7 +36,32 @@ variable "mks_shared" {
   type = string
 }
 variable "mks_preset" {
+  type = bool
+}
+variable "cloudfront_dashbaord_name" {
   type = string
 }
+variable "cloudfront_owner_name" {
+  type = string
+}
+variable "cloudfront_shared" {
+  type = string
+}
+variable "cloudfront_preset" {
+  type = bool
+}
+variable "elasticsearch_dashbaord_name" {
+  type = string
+}
+variable "elasticsearch_owner_name" {
+  type = string
+}
+variable "elasticsearch_shared" {
+  type = string
+}
+variable "elasticsearch_preset" {
+  type = bool
+}
+
 
 
